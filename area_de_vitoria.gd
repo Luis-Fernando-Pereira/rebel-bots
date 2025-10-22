a97a38891653e57fdee641deaacf023f972ff929
miguettohuertas <miguetto1314@gmail.com>
2025-10-21 22:42:26 -0300

    nivel03
    Nivel03 quase pronto, somente falta ajustar um pouco o layout.

--- area_de_vitoria.gd
+++ area_de_vitoria.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("vitoria"): 
-		get_tree().change_scene_to_file("res://app/scenes/ui/vitoria.tscn")
+		get_tree().change_scene_to_file("res://app/scenes/levels/nivel02.tscn")
